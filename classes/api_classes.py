7e5246b425833a0ffc7a233a484d300566947833
scroco158 <scroco@mail.ru>
2024-01-27 09:11:25 +0300

    классы работы с API

--- classes/api_classes.py
+++ classes/api_classes.py
@@ -6,3 +6,92 @@
         б) фильтрация данных
 """
 
+from abc import ABC, abstractmethod
+import requests
+import json
+import os
+
+
+class AbstractAPI(ABC):
+    @abstractmethod
+    def get_vacancies(self, key_word):
+        pass
+
+    @abstractmethod
+    def filter_vacancies(self, key_words):
+        pass
+
+
+class Hh(AbstractAPI):
+
+    def get_vacancies(self, key_word):
+        """
+        Получает список вакансий по ключевому слову key_word
+        и возвращает словарь с нефильтрованными вакансиями
+        """
+
+        hh_url = "https://api.hh.ru/vacancies?text="+key_word
+        response = requests.get(url=hh_url).json()
+
+        return response['items']
+
+    def filter_vacancies(self, key_word):
+        """
+        Отбираем необходимые для работы поля и возвращаем словарь с
+        отфильтрованной информацией по вакансиям
+        """
+        vacancies = self.get_vacancies(key_word)
+        filtered_vacancies = []
+        for vacancy in vacancies:
+            s_f = 0
+            s_t = 0
+            if vacancy["salary"] is not None:
+                if vacancy["salary"]["from"] is not None:
+                    s_f = vacancy["salary"]["from"]
+                if vacancy["salary"]["to"] is not None:
+                    s_t = vacancy["salary"]["to"]
+
+            filtered_vacancies.append({
+                "name": vacancy["name"],
+                "url": vacancy["url"],
+                "salary_from": s_f,
+                "salary_to": s_t,
+                "description": vacancy["snippet"]["responsibility"]})
+
+        return filtered_vacancies
+
+
+class Sj(AbstractAPI):
+
+    def get_vacancies(self, key_word):
+
+        """
+        Получает список вакансий по ключевому слову key_word
+        и возвращает словарь с нефильтрованными вакансиями
+        SJ_API_KEY
+        """
+
+        sj_api_key = os.environ.get('SJ_API_KEY')
+        print("ключ", sj_api_key)
+
+        headers = {"X-Api-App-Id": sj_api_key}
+        params = {"keyword": key_word}
+        response = requests.get("https://api.superjob.ru/2.0/vacancies/", params=params, headers=headers).json()
+        return response['objects']
+
+    def filter_vacancies(self, key_word):
+        """
+        Отбираем необходимые для работы поля и возвращаем словарь с
+        отфильтрованной информацией по вакансиям
+        """
+        vacancies = self.get_vacancies(key_word)
+        filtered_vacancies = []
+        for vacancy in vacancies:
+            filtered_vacancies.append({
+                "name": vacancy["profession"],
+                "url": vacancy["link"],
+                "salary_from": vacancy["payment_from"],
+                "salary_to": vacancy["payment_to"],
+                "description": vacancy["work"]})
+
+        return filtered_vacancies
